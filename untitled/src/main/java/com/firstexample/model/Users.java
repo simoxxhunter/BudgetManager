3082c553303f7d1cd4aa722f20fc21d68851a00c
simoxxhunter <94250469+simoxxhunter@users.noreply.github.com>
2024-05-16 15:52:19 +0000

    creation table & insertion. config hibernate

---
+++ untitled/src/main/java/com/firstexample/model/Users.java
@@ -0,0 +1,32 @@
+package com.firstexample.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String name;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
